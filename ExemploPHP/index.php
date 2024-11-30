9538d183bae0c325159b6aaaf6665326e58faad9
leonardodeandrade91 <leonardodeandrade91@hotmail.com>
2024-11-30 14:28:22 -0300

    Primeiro Commit

---
+++ ExemploPHP/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Exemplo PHP</title>
+    </head>
+    <body>
+        <?php
+            
+        ?>
+    </body>
+</html>
